7ea8d32ee82355e1f59180f7726c8264ca448af6
the-chronicles <oluseungideon@gmail.com>
2024-01-01 23:36:07 +0100

    staying logged in

--- store/auth-context.js
+++ store/auth-context.js
@@ -1,4 +1,5 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 export const AuthContext = createContext({
   token: "",
@@ -10,12 +11,16 @@ export const AuthContext = createContext({
 function AuthContextProvider({ children }) {
   const [authToken, setAuthToken] = useState();
 
+   
+
   function authenticate(token) {
     setAuthToken(token);
+    AsyncStorage.setItem("token", token);
   }
 
   function logout() {
     setAuthToken(null);
+    AsyncStorage.removeItem('token')
   }
 
   const value = {
